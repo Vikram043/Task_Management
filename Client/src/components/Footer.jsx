e037c581d8885972ad8575b9bdbaad6818d0194f
Vikram Naik <119391188+Vikram043@users.noreply.github.com>
2023-10-26 11:06:41 +0530

    Update Footer.jsx

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-200 p-4">
       <div className="container mx-auto text-center">
-        <p className="text-gray-600">© 2023 My App. All rights reserved.</p>
+       <p className="text-gray-600">Made with <span style="color: red;">&#9829;</span> by Vikram. © 2023 My App. All rights reserved.</p>
       </div>
     </footer>
   );
